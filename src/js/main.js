9d58672e305a0139c256cfbe7ad10f254a9e1793
Császár Nagy Márk <mark.csaszarnagy@gmail.com>
2026-05-18 23:51:01 +0200

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+
+const menuButton = document.querySelector('.js-menu-button');
+const offcanvasMenu = document.querySelector('.js-offcanvas');
+const overlay = document.querySelector('.js-overlay');
+
+menuButton?.addEventListener('click', () => {
+  offcanvasMenu.classList.toggle('is-open');
+  overlay.classList.toggle('is-visible');
+});
+
+overlay?.addEventListener('click', () => {
+  offcanvasMenu.classList.remove('is-open');
+  overlay.classList.remove('is-visible');
+});
