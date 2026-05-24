f7437ac81f8fe14ac89845fd646c121e0b54c621
Császár Nagy Márk <mark.csaszarnagy@gmail.com>
2026-05-24 21:30:04 +0200

    readme3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/pepita/', 
+  base: '/pepita/',
   build: {
-    outDir: 'docs', 
+    outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tickets: resolve(__dirname, 'tickets.html'),
+      },
+    },
   },
 });
